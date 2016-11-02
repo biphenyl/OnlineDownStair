fe6a761f1d79d68fc35e51c448e297f53404c44d
biphenyl <johnny60508@gmail.com>
2016-11-02 11:19:50 +0800

    introduction first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+	$('.intro').hide(),
+	$('#empty_intro').show();
+});
+
+$(document).ready(function(){
+	$("#area_1").mouseover(function(){
+		$("#intro_1").fadeIn(500);
+	})
+});
+$(document).ready(function(){
+	$("#area_1").mouseout(function(){
+		$("#intro_1").fadeOut(500);
+	})
+});
+
+$(document).ready(function(){
+	$("#area_2").mouseover(function(){
+		$("#intro_2").fadeIn(500);
+	})
+});
+$(document).ready(function(){
+	$("#area_2").mouseout(function(){
+		$("#intro_2").fadeOut(500);
+	})
+});
+
+$(document).ready(function(){
+	$("#area_3").mouseover(function(){
+		$("#intro_3").fadeIn(500);
+	})
+});
+$(document).ready(function(){
+	$("#area_3").mouseout(function(){
+		$("#intro_3").fadeOut(500);
+	})
+});
+
+$(document).ready(function(){
+	$("#area_4").mouseover(function(){
+		$("#intro_4").fadeIn(500);
+	})
+});
+$(document).ready(function(){
+	$("#area_4").mouseout(function(){
+		$("#intro_4").fadeOut(500);
+	})
+});
