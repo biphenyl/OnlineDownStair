1c6ee6a160b5508ef3296d9154acd96b25d0a9c0
biphenyl <johnny60508@gmail.com>
2016-12-04 14:17:44 +0800

    hp bar, back ground and more

--- game/game.js
+++ game/game.js
@@ -2,38 +2,64 @@
 const gameWidth = 2000;
 const gameHeight = 600;
 
+const upBound = 0;
+const leftBound = 0;
+const rightBound = 2000;
+
 const platformWidth = 300;
 const platformHeight = 64;
 const platformUpSpeed = -120;
 
-const playerHeight = 32;
+const playerHeight = 48;
+const playerWidth = 32;
 const playerSpeedX = 200;
 const playerGravity = 400;
 
-var windowWidth = $(window).width();
+const maxHp = 10;
 
+var windowWidth = $(window).width();
 var game = new Phaser.Game(windowWidth, gameHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update , render: render});
 
 function preload() {
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
+    game.load.image('sky', 'assets/background/inner.png');
+    game.load.image('platformGreen', 'assets/platform/platformGreen.png');
+    game.load.image('platformGray', 'assets/platform/platformGray.png');
+    game.load.image('platformLeft', 'assets/platform/platformLeft.png');
+    game.load.image('platformRight', 'assets/platform/platformRight.png');
+    game.load.image('platformOrange', 'assets/platform/platformOrange.png');
     game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('dude1', 'assets/guys/guy1.png', 32, 48);
+    game.load.spritesheet('dude2', 'assets/guys/guy2.png', 32, 48);
+
+    game.load.image('hp1', 'assets/HP/hp1.png');
+    game.load.image('hp2', 'assets/HP/hp2.png');
+    game.load.image('hp3', 'assets/HP/hp3.png');
+    game.load.image('hp4', 'assets/HP/hp4.png');
+    game.load.image('hp5', 'assets/HP/hp5.png');
+    game.load.image('hp6', 'assets/HP/hp6.png');
+    game.load.image('hp7', 'assets/HP/hp7.png');
+    game.load.image('hp8', 'assets/HP/hp8.png');
+    game.load.image('hp9', 'assets/HP/hp9.png');
+    game.load.image('hp10', 'assets/HP/hp10.png');
 
 }
 
 var userID;
-var characters = new Array(20);
 var playerCount = 1;
-var platforms;
+
+// characters is our array store our data(hp, score...etc), players is a phaser group that do physic thing
+var characters = new Array(20);
 var players;
+
+// stairs is our array store our data(type), platforms is a phaser group that do physic thing
+var stairs = new Array();
+var platforms;
+var platformName = ["platformGreen", "platformGray", "platformLeft", "platformRight", "platformOrange"];
+
 var cursors;
 var ground;
-
-var topBound;
-var leftBound;
-var rightBound;
+var background;
 
 var graphics;
 
@@ -51,7 +77,10 @@ function character(newID, newName, posX, posY)
     this.id = newID;
     this.name = newName;
     
-    this.player = players.create(posX, posY, 'dude');
+    if(this.id%2 == 0)    
+        this.player = players.create(posX, posY, 'dude1');
+    else
+        this.player = players.create(posX, posY, 'dude2');
     initPlayer(this.player);
 
     //set text's center at top of player
@@ -59,14 +88,27 @@ function character(newID, newName, posX, posY)
     this.nameText.x = this.player.centerX - this.nameText.width / 2;
 
     this.hp = 10;
+    this.lastHp = 10;
+    this.hpBar = game.add.image(this.player.x, this.player.y-26, 'hp10');
+
     this.score = 0; 
     
     // select what character looks like
     this.face = 0;
 
+    // status of character, -1 as flying(not on platform), and postive number is which platform this character stand on
     this.standOn = 0;
 }
 
+function stair(x, newType)
+{   
+    //console.log("new type is " + newType); 
+    
+    this.platform = platforms.create(x, gameHeight, platformName[newType]);
+    this.type = newType;
+
+    initPlatform(this.platform);
+}
 //
 function sentCharacter(id)
 {
@@ -91,8 +133,8 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
-    //background = game.add.sprite(0, 0, 'sky');
-    //background.scale.setTo($(window).width()/800, gameHeight/600);
+    background = game.add.sprite(0, 0, 'sky');
+    background.scale.setTo(gameWidth/background.width, gameHeight/background.height);
 
     game.world.setBounds(0, 0, gameWidth, gameHeight);
 
@@ -108,7 +150,7 @@ function create() {
     platforms.enableBody = true;
 
     // Here we create the ground.
-    ground = platforms.create(0, game.world.height - 64, 'ground');
+    ground = platforms.create(0, game.world.height - 64, 'platformGreen');
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     //ground.scale.setTo(2, 2);
@@ -118,11 +160,11 @@ function create() {
     //ground.body.velocity.y = -10;
 
     //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    var ledge = platforms.create(400, 400, 'platformGreen');
     ledge.body.immovable = true;
     //ledge.body.velocity.y = -10;
 
-    ledge = platforms.create(-150, 250, 'ground');
+    ledge = platforms.create(-150, 250, 'platformGreen');
     ledge.body.immovable = true;
     //ledge.body.velocity.y = -10;
 
@@ -153,6 +195,7 @@ function create() {
     //  Finally some stars to collect
     //  The score
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText.fixedToCamera = true;
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
@@ -163,7 +206,6 @@ function create() {
     })
 
     game.time.events.loop(Phaser.Timer.SECOND * 1, createNewPlatform, this);
-    game.time.events.loop(Phaser.Timer.SECOND * 0.2, showNewSize, this);
     game.time.events.loop(Phaser.Timer.SECOND * 0.03, mainLoop, this);
 
     game.time.events.loop(Phaser.Timer.SECOND * 5, playerJoin, this);
@@ -182,26 +224,25 @@ function update() {
     game.physics.arcade.collide(players, rightBound);
     */
     
-    //  Collide the player and the stars with the platforms
+    //  Collide the player and the stars with the platforms , when a platform too high to  player stand on it without beyond the upBound, it can be pass through 
     platforms.forEach(function(item){
-        if(item.top > 32)
-            game.physics.arcade.collide(players, platforms, standOnPlatform, null, this);
+        if(item.top > playerHeight)
+        {
+            for(i=0; i<playerCount; ++i)
+                game.physics.arcade.collide(characters[i].player, platforms, standOnPlatform, null, this);
+        }
     })
 
     //each player should collide
     game.physics.arcade.collide(players);
 
-
-    
     for(i=0; i<playerCount; i++)
-        for(j=i+1; j<playerCount; j++)
+        for(j=0; j<playerCount; j++)
+        {
+            if(i==j)
+                continue;    
             avoidStack(characters[i].player, characters[j].player);
-    for(i=0; i<playerCount; i++)
-        for(j=i+1; j<playerCount; j++)
-            avoidStack(characters[i].player, characters[j].player);
-    for(i=0; i<playerCount; i++)
-        for(j=i+1; j<playerCount; j++)
-            avoidStack(characters[i].player, characters[j].player);
+        }
     
     /*
     for(i=0; i<playerCount; i++)
@@ -244,17 +285,23 @@ function update() {
             player.frame = 4;
         }
         
-        //  Allow the player to jump if they are touching the ground.
+        //jump
+        /*
         if (cursors.up.isDown && player.body.touching.down)
         {
             player.body.velocity.y = -350;
         }
-
+        */
         //Let ID follow player
         if(player.alive)
         {
             characters[i].nameText.x = player.centerX - characters[i].nameText.width/2;
             characters[i].nameText.y = player.y - 16;
+
+            characters[i].hpBar.x = player.x;
+            characters[i].hpBar.y = player.y-26;
+
+            characters[i].hpBar.loadTexture('hp' + characters[i].hp);
         }
     }
 
@@ -291,17 +338,21 @@ function update() {
             player.x = gameWidth - player.width;
             player.body.velocity.x = 0;
         }
+
     }
     
     // Let scoreText always at left-top
-    scoreText.x = game.camera.x + 16;
-    scoreText.y = game.camera.y + 16;
+    //scoreText.x = game.camera.x + 16;
+    //scoreText.y = game.camera.y + 16;
       
 
     platforms.forEach(function(item)
     {
         if(item.top <= -64)
+        {   
             platforms.remove(item);
+            stairs.shift();
+        }
     })
 
 
@@ -309,17 +360,18 @@ function update() {
     for(i=0; i<playerCount; i++)
     {
         player = characters[i].player;
-        if((player.y+32 > gameHeight) && player.alive)
+        if((player.y > gameHeight) && player.alive)
          {  
             player.kill();
          
             console.log("player " + characters[i].name + " die~");
-            
-            score = 0;
 
             //make id invisable
             characters[i].nameText.x = -100;
             characters[i].nameText.y = -100;
+
+            characters[i].hpBar.x = -100;
+            characters[i].hpBar.y = -100;
 
 
 
@@ -328,6 +380,8 @@ function update() {
 
             if(i==userID)
             {
+                player.hp = 0;
+                player.score = 0;
                 scoreText.text = "Revive in 3 sec";
                 reviveEvent = game.time.events.loop(Phaser.Timer.SECOND * 1, reviveCount, this);
                 reviveCounter = 3;
@@ -346,7 +400,6 @@ function update() {
         }
     }
 
-
     //console.log("x is " + characters[userID].player.x);
 }
 
@@ -354,20 +407,6 @@ function render()
 {
     //game.debug.spriteCoords(player, 32, 500);
 }
-/*
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-
-
-
-}
-*/
 
 $(window).resize(function(){
     windowWidth = $(window).width();
@@ -381,35 +420,38 @@ function standOnPlatform(player, platform)
     if(player.y + player.body.height == platform.y)
     {
         score += 1;
-        scoreText.text = "score: " + score;
-        
+        scoreText.text = "score: " + score;        
     }
 
 }
 
+function createMap()
+{
+    //count platform number
+
+    //create them all
+}
+
 function createNewPlatform()
 {
+    //listen server for new platform
     newPlatformX = Math.random() * (gameWidth - platformWidth);
     while(newPlatformX < lastPlatformX + platformWidth && newPlatformX > lastPlatformX - platformWidth)
         newPlatformX = Math.random() * (gameWidth - platformWidth);
-    var ledge = platforms.create(newPlatformX, gameHeight, 'ground');
-    ledge.body.immovable = true;
-    ledge.body.velocity.y = platformUpSpeed;
+
+    newType = Math.floor((Math.random() * 5));
+    stairs.push(new stair(newPlatformX, newType));
 
     lastPlatformX = newPlatformX;
-}
 
-function showNewSize()
-{
-    
+    console.log(platforms.length);
+    console.log(stairs.length);
 }
 
 function mainLoop()
 {
     //render everyone's position
     //send keyboard status
-
-
 }
 
 function drawBound()
@@ -417,6 +459,20 @@ function drawBound()
     graphics.lineStyle(5, 0xFF00FF, 1);
     graphics.drawRect(0, 0, gameWidth, gameHeight);
 }
+
+// don't use draw to display hp, I think
+/*
+function drawHp()
+{
+    for(int i=0; i<playerCount; i++)
+    {
+        player = character[i].player;
+        
+        graphics.lineStyle(2, 0x00000, 1);
+        graphics.drawRect(player.x, player.y, player.height, player.width);
+    }
+}
+*/
 
 function reviveCount()
 {
@@ -468,7 +524,7 @@ function playerJoin()
     playerCount++;
 }
 
-function initPlayer(player, posX, posY)
+function initPlayer(player)
 {
     game.physics.arcade.enable(player);
     player.body.gravity.y = playerGravity;
@@ -477,6 +533,12 @@ function initPlayer(player, posX, posY)
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     player.body.maxVelocity = new Phaser.Point(200,300);
+}
+
+function initPlatform(platform)
+{
+    platform.body.immovable = true;
+    platform.body.velocity.y = platformUpSpeed;
 }
 
 function avoidStack(player1, player2)
