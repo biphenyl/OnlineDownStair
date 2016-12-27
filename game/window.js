0f1c15bb8d9d186b8dc8150bb77f938fd8d52946
benwu95 <benwu95@gmail.com>
2016-12-27 23:19:37 +0800

    set scale about mobile, change refresh button text

--- game/window.js
+++ game/window.js
@@ -6,14 +6,18 @@ $(document).ready(function()
 	windowHeight = $(window).height();
 	if(windowHeight > 600)
 		$("#room").css("margin-top", (windowHeight - 600) / 2 );
-})
+  if(windowHeight < 600) {
+    $('#myViewport').attr({"initial-scale": 0.25, "maximum-scale": 0.25});
+  }
+});
 
 $(window).resize(function()
 {
 	windowHeight = $(window).height();
 	if(windowHeight > 600)
 		$("#room").css("margin-top", (windowHeight - 600) / 2 );	
-	else
+	else {
 		$("#room").css("margin-top", 0 );
-	
-})
+    $('#myViewport').attr({"initial-scale": 0.25, "maximum-scale": 0.25});
+	}
+});
